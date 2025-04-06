3ff7281293d36246156469621a2aa2cf1421c269
Rishik Ramena <ramenarishik@gmail.com>
2025-04-06 13:53:05 +0530

    Initial Commit

---
+++ GoogleSheetUtils.js
@@ -0,0 +1,26 @@
+// Utility to update a Google Sheet cell
+function updateGoogleSheetCell(sheetId, rowNumber, columnNumber, value) {
+  var sheet = SpreadsheetApp.openById(sheetId).getActiveSheet();
+
+  if (isNaN(rowNumber) || rowNumber <= 0) {
+    console.log("Error: Invalid row number:", rowNumber);
+    return;
+  }
+
+  try {
+    sheet.getRange(rowNumber, columnNumber).setValue(value);
+  } catch (error) {
+    console.log("Error updating sheet:", error.message);
+  }
+}
+
+// Utility to append a row to a Google Sheet
+function appendRowToGoogleSheet(sheetId, rowData) {
+  var sheet = SpreadsheetApp.openById(sheetId).getActiveSheet();
+
+  try {
+    sheet.appendRow(rowData);
+  } catch (error) {
+    console.log("Error appending row to sheet:", error.message);
+  }
+}
