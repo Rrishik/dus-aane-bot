246cfaed4895ee08b1e44e200b3d97521af4af5d
Rishik <ramenarishik@gmail.com>
2025-04-12 01:37:31 +0530

    test

--- Constants.js
+++ Constants.js
@@ -1,7 +1,6 @@
 // Debug flag
 const DEBUG = true;
 
-
 const CHAT_ID = DEBUG ? PERSONAL_CHAT_ID : GROUP_CHAT_ID; // Use Rishik's chat ID for debugging
 const BOT_UPDATE_URL = `https://api.telegram.org/bot${BOT_TOKEN}/getUpdates`;
 const BOT_EDIT_MESSAGE_URL = `https://api.telegram.org/bot${BOT_TOKEN}/editMessageText`;
