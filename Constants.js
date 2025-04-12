78685d3b990c70ecfb9493a7b67baa6c9fde0740
Rishik <ramenarishik@gmail.com>
2025-04-12 17:00:12 +0530

    updating deployment script

--- Constants.js
+++ Constants.js
@@ -6,9 +6,13 @@ const BOT_UPDATE_URL = `https://api.telegram.org/bot${BOT_TOKEN}/getUpdates`;
 const BOT_EDIT_MESSAGE_URL = `https://api.telegram.org/bot${BOT_TOKEN}/editMessageText`;
 const BOT_SEND_MESSAGE_URL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
 const BOT_ANSWER_CALLBACK_QUERY_URL = `https://api.telegram.org/bot${BOT_TOKEN}/answerCallbackQuery`;
+const BOT_ADD_TRANSACTION_KEYWORD = 'addTransaction';
+
 
 // Google Sheets
 const SHEET_ID = DEBUG ? TEST_SHEET_ID : PROD_SHEET_ID;
+const SPLIT_COLUMN = 8; // Column H
+
 
 // Gmail
 const MAILS_LOOKBACK_PERIOD = DEBUG ? '1d' : '1h';
